eb097b8db4c8be81e0009611eab7154c2d083033
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-10-13 18:07:13 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Tabs, Layout, Input, List } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+const { TextArea } = Input;
+
+const { TabPane } = Tabs;
+
+const data = [
+  "Scenario 1",
+  "Scenario 2",
+  "Scenario 3",
+  "Scenario 4",
+  "Scenario 5",
+];
+
+const initialPanes = [
+  { title: "Tab 1", content: "Content of Tab 1", key: "1" },
+  { title: "Tab 2", content: "Content of Tab 2", key: "2" },
+  {
+    title: "Tab 3",
+    content: "Content of Tab 3",
+    key: "3",
+    closable: false,
+  },
+];
+
+class Demo extends React.Component {
+  newTabIndex = 0;
+
+  state = {
+    activeKey: initialPanes[0].key,
+    panes: initialPanes,
+  };
+
+  onChange = (activeKey) => {
+    this.setState({ activeKey });
+  };
+
+  onEdit = (targetKey, action) => {
+    this[action](targetKey);
+  };
+
+  add = () => {
+    const { panes } = this.state;
+    const activeKey = `newTab${this.newTabIndex++}`;
+    const newPanes = [...panes];
+    newPanes.push({
+      title: "New Tab",
+      content: "Content of new Tab",
+      key: activeKey,
+    });
+    this.setState({
+      panes: newPanes,
+      activeKey,
+    });
+  };
+
+  remove = (targetKey) => {
+    const { panes, activeKey } = this.state;
+    let newActiveKey = activeKey;
+    let lastIndex;
+    panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1;
+      }
+    });
+    const newPanes = panes.filter((pane) => pane.key !== targetKey);
+    if (newPanes.length && newActiveKey === targetKey) {
+      if (lastIndex >= 0) {
+        newActiveKey = newPanes[lastIndex].key;
+      } else {
+        newActiveKey = newPanes[0].key;
+      }
+    }
+    this.setState({
+      panes: newPanes,
+      activeKey: newActiveKey,
+    });
+  };
+
+  render() {
+    const { panes, activeKey } = this.state;
+    return (
+      <div>
+        <Layout>
+          <Header>
+            <Tabs
+              type="editable-card"
+              onChange={this.onChange}
+              activeKey={activeKey}
+              onEdit={this.onEdit}
+            >
+              {panes.map((pane) => (
+                <TabPane
+                  tab={pane.title}
+                  key={pane.key}
+                  closable={pane.closable}
+                >
+                  {pane.content}
+                </TabPane>
+              ))}
+            </Tabs>
+          </Header>
+          <Layout>
+            <Sider width={350}>
+              <TextArea rows={25} />
+            </Sider>
+            <Content>Tree</Content>
+            <Sider>
+              <List
+                header={<div>Header</div>}
+                footer={<div>Footer</div>}
+                bordered
+                dataSource={data}
+                renderItem={(item) => (
+                  <List.Item>{item}</List.Item>
+                )}
+              />
+            </Sider>
+          </Layout>
+          <Footer>Footer</Footer>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default Demo;
